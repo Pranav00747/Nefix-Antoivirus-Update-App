348043ebc651cd64b95691ce510517238b5fe26f
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-03-11 04:23:40 +0530

    Add files via upload

---
+++ netfixupdate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace netfixupd
+{
+    public partial class netfixupdate : Form
+    {
+        #region netfixupd code
+
+        public netfixupdate()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelbutton_Click(object sender, EventArgs e)
+        {
+            (sender as Button).Parent.Dispose();
+        }
+
+        #endregion
+
+
+    }
+}
